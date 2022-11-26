9c39618ff18fdd5139d59a192ea69823ef6b21d3
CharmingEffect <arkadiuszgrudzien@yahoo.com>
2022-11-26 13:58:11 +0000

    it send the email to activate the account

--- backend/src/main/java/com/arkadiusgru/cls/registration/RegistrationController.java
+++ backend/src/main/java/com/arkadiusgru/cls/registration/RegistrationController.java
@@ -1,24 +1,33 @@
 package com.arkadiusgru.cls.registration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping("/api/admin/registration")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class RegistrationController {
 
     private final RegistrationService registrationService;
 
-    @PostMapping
+    @PostMapping(value = "/admin/registration")
     public String register(@RequestBody RegistrationRequest request) {
 
         return registrationService.register(request);
+    }
+
+    @GetMapping(path = "/register-confirm")
+    public String confirm(@RequestParam("token") String token) {
+
+        return registrationService.confirmToken(token);
+
     }
 
 }
