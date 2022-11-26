a454725aff9c72dc1ed421af534618a13e7cb4fa
CharmingEffect <arkadiuszgrudzien@yahoo.com>
2022-11-26 16:27:53 +0000

    changes

--- backend/src/main/java/com/arkadiusgru/cls/ClsApplication.java
+++ backend/src/main/java/com/arkadiusgru/cls/ClsApplication.java
@@ -2,8 +2,10 @@ package com.arkadiusgru.cls;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationProperties(prefix = "pinnacle.email")
 public class ClsApplication {
 
 	public static void main(String[] args) {
